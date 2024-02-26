1286531dd9b56797193367459cf2bbb58ccbdbf0
KajalKolhe <kajalkolhe4@gmail.com>
2024-02-26 13:54:03 +0530

    Update And Delete Category

---
+++ src/main/java/com/sheryians/major/repoitory/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.sheryians.major.repoitory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sheryians.major.model.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer>{
+
+}
