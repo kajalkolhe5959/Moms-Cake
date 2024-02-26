ea5b4be7140b070a9fa7026f76a63ce06ba2fad0
KajalKolhe <kajalkolhe4@gmail.com>
2024-02-26 10:39:43 +0530

    Categories add section

--- src/main/java/com/sheryians/major/MajorApplication.java
+++ src/main/java/com/sheryians/major/MajorApplication.java
@@ -8,6 +8,7 @@ public class MajorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MajorApplication.class, args);
+		System.out.println("Project Start...");
 	}
 
 }
